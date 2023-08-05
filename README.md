1b657d8f1c0c4a1cd5147efbda78b62a7007b189
Martyn147 <mrosero.sfq@gmail.com>
2023-08-05 02:46:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ubicacion Xamarin Prueba
+ 
---
+++ ubicacion/ubicacion/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace ubicacion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetCurrentLocation();
+        }
+
+        private async void OnMyLocationButtonClicked(object sender, EventArgs e)
+        {
+            GetCurrentLocation();
+        }
+
+        private async void GetCurrentLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.High);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    var position = new Position(location.Latitude, location.Longitude);
+                    map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMiles(1)));
+                }
+            }
+            catch (FeatureNotSupportedException)
+            {
+                // La geolocalización no es compatible en este dispositivo
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+            }
+        }
+    }
+}
